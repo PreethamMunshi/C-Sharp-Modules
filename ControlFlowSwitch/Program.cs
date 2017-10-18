417c903c6874f248e56770306c6c43d00a747e4b
Preetham Munshi <munshipreetham@gmail.com>
2017-10-18 14:34:03 -0500

    Control Flow statements
    Adding Control Flow decision and switch statements

---
+++ ControlFlowSwitch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlFlowSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "Preetham":
+                    Console.WriteLine("This is me");
+                    break;
+                case "VS":
+                    Console.WriteLine("This is my IDE");
+                    break;
+                default:
+                    Console.WriteLine("No case match");
+                    break;
+
+            }
+        }
+    }
+}
