07880feb6bc687c42fe134b3f322e88ec8585393
Preetham Munshi <munshipreetham@gmail.com>
2017-10-18 13:44:00 -0500

    Variable and Expressions and Operators
    Creating new project expressions and operators.

---
+++ ExpressionsAndOperators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionsAndOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int simpleExpression1 = 5 - 10;
+            int simpleExpression2 = 8 * 2;
+            int complexExpression = (((6 * 7) + 5) + simpleExpression1) / simpleExpression2;
+
+            Console.WriteLine(simpleExpression1);
+            Console.WriteLine(simpleExpression2);
+            Console.WriteLine(complexExpression);
+
+            Console.ReadLine();
+        }
+    }
+}
--- Variable/Variable/Program.cs
+++ Variable/Variable/Program.cs
@@ -10,6 +10,19 @@ namespace Variable
     {
         static void Main(string[] args)
         {
+            int CountofBooks = 1;
+            string myName = "Preetham";
+            bool isCompatible = true;
+            string number = "2";
+            int numberConverted = Convert.ToInt32(number);
+
+            Console.WriteLine(CountofBooks);
+            Console.WriteLine(myName);
+            Console.WriteLine(isCompatible);
+            Console.WriteLine(number);
+            Console.WriteLine(numberConverted);
+
+            Console.ReadLine();
         }
     }
 }
